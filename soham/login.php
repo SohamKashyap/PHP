367b10d0b8791c907e137b34fcceac1f8d4f46dc
SohamKashyap <153927096+SohamKashyap@users.noreply.github.com>
2023-12-29 15:25:58 +0530

    login.php

--- soham/login.php
+++ soham/login.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,22 +43,73 @@
     </div>
   </div>
 </nav>
-<div class="container mt-4">
-    <h1> Login using your Input Data </h1>
-<form action="/soham/form.php"  method="post" >
-  <div class="mb-3">
-    <label for="email" class="form-label">Email address</label>
-    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="pass" class="form-label">Password</label>
-    <input type="password" class="form-control" id="pass" name="pass">
-  </div>
-  <button type="button" class="btn btn-success">Log In</button>
-  <button href="form.php" type="submit" class="btn btn-primary">Sign Up</button>
 
-</form>
+<div class="container mt-4">
+    <h1>Login using your Input Data</h1>
+    <?php
+    // PHP code for login validation
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "form";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $email = $_POST['email'];
+        $stmt = $conn->prepare("SELECT * FROM registraction WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_assoc();
+            $hashed_password_from_db = $row['Password']; // Replace 'Password' with your column name
+            $entered_password = $_POST['pass']; // Get the password entered during login
+            if (password_verify($entered_password, $hashed_password_from_db)) {
+              echo '<div id="alertMsg" class="alert alert-success alert-dismissible fade show" role="alert">
+              <strong>Login successfully!</strong>  
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+            } else {
+              echo '<div id="alertMsg" class="alert alert-danger alert-dismissible fade show" role="alert">
+              <strong>Incorrect Password! Enter Again !</strong>  
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+            }
+        } else {
+          echo '<div id="alertMsg" class="alert alert-danger alert-dismissible fade show" role="alert">
+          <strong>User With this Email Does not Exist , Enter Again !</strong>  
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+        } 
+        
+        $stmt->close();
+        $conn->close();
+    }
+    ?>
+    <script>
+        setTimeout(function() {
+            document.getElementById('alertMsg').style.display = 'none';
+        }, 5000); // 5000 milliseconds = 5 seconds
+    </script>
+
+    <form action="/soham/login.php" method="post">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="pass" class="form-label">Password</label>
+            <input type="password" class="form-control" id="pass" name="pass">
+        </div>
+        <button type="submit" class="btn btn-success">Log In</button>
+        <a href="/soham/form.php" class="btn btn-primary">Sign Up</a>
+    </form>
 </div>
 </body>
 </html>
